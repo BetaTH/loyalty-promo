76d0242047ca357ee4366eaa4fe8ef756356c633
Thielson Almendra <thielson12@gmail.com>
2024-07-07 02:57:57 -0300

    feat: add preload images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,58 +1,60 @@
-import type { Metadata } from 'next'
-import { Inter, Play } from 'next/font/google'
-import './globals.css'
-import { ThemeProvider } from '@/components/them-provider'
-import { cn } from '@/lib/utils/cn'
-import localFont from 'next/font/local'
-import { Toaster } from '@/components/ui/toaster'
+import type { Metadata } from "next";
+import { Inter, Play } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/them-provider";
+import { cn } from "@/lib/utils/cn";
+import localFont from "next/font/local";
+import { Toaster } from "@/components/ui/toaster";
+import { PreloadImages } from "@/components/preload/preload-images";
 
 const inter = Inter({
-  subsets: ['latin'],
-  variable: '--font-inter',
-})
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
 
 const play = Play({
-  subsets: ['latin'],
-  weight: ['400', '700'],
-})
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
 
 export const metadata: Metadata = {
-  title: 'Promoção Fidelidade',
-  description: 'Promoção Fidelidade da SA Suplementos',
-}
+  title: "Promoção Fidelidade",
+  description: "Promoção Fidelidade da SA Suplementos",
+};
 
 const supermolot = localFont({
-  src: '../fonts/tt-supermolot-neue-extrabold.ttf',
-  display: 'swap',
-  variable: '--font-supermolot',
-})
+  src: "../fonts/tt-supermolot-neue-extrabold.ttf",
+  display: "swap",
+  variable: "--font-supermolot",
+});
 const bankGothic = localFont({
-  src: '../fonts/bank-gothic-light-bt.ttf',
-  display: 'swap',
-  variable: '--font-bank-gothic',
-})
+  src: "../fonts/bank-gothic-light-bt.ttf",
+  display: "swap",
+  variable: "--font-bank-gothic",
+});
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="pt" suppressHydrationWarning>
       <body
         className={cn(
-          'bg-background ',
+          "bg-background ",
           play.className,
           bankGothic.variable,
           supermolot.variable,
-          inter.variable,
+          inter.variable
         )}
       >
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <PreloadImages />
           {children}
         </ThemeProvider>
         <Toaster />
       </body>
     </html>
-  )
+  );
 }
